1cc00e2632aa42a4f9014e095e0221cc22c82f7c
LucasBertrand <lucas.bertrand3@etu.unistra.fr>
2015-04-21 15:52:26 +0200

    Update the stockTwiit function

--- twiit/stockTwiit.js
+++ twiit/stockTwiit.js
@@ -1,15 +1,20 @@
-var fs = require('fs');
+var fs = require( 'fs' );
 
-module.exports = function (data, callback)
+/* Receive twiit's data from the client, create a txt file and store it 
+ * in ./twiit/data
+ * The name of the files is the timestamp of submitted twiit
+ * 
+ * @param callback {Function} the callback function
+ */
+module.exports = function ( data, callback )
 {
 	
 	var path = __dirname+ "/data/" + Date.now()+ ".txt";
 	var content = "<" + data.user_name + ">" + data.twiit;
-	fs.writeFile(path, content, function(error)
-		{
-			if (error) throw error
-			callback(error)
-		});
+	fs.writeFile( path, content, function( error )
+	{		
+		callback( error )
+	});
 	
 	
 };
