a4c0c4522ac0bc63cd17b8c4c691253bef53f325
LucasBertrand <luk.bertrand@gmail.com>
2015-04-21 16:16:29 +0200

    Update of client javascript main file

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,12 @@
 var response = document.getElementById("response");
 
 if(response!=undefined){
-if(response.innerHTML=="Success")
-{
-	response.style.backgroundColor="#66FF33";
-}
-if(response.innerHTML=="Fail")
-{
-	response.style.backgroundColor="#FE3A3C";
+	if(response.innerHTML=="Success")
+	{
+		response.style.backgroundColor="#66FF33";
+	}
+	if(response.innerHTML=="Fail")
+	{
+		response.style.backgroundColor="#FE3A3C";
 }
 }
