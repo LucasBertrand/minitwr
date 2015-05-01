3aa3d0035247a529e250a75ddcfd6f5ca3c7ad9a
VConstans <victor.constans@etu.unistra.fr>
2015-05-01 13:26:21 +0200

    Add websocket client

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,22 +1,2 @@
-/*var response = document.getElementById("response");
-
-
-var res = false;
-
-	if(res)	{
-		response.classList.add("success");
-	}
-	else	{
-		response.classList.add("fail");
-	}
-
-
-if(response!=undefined){
-	if(response.innerHTML=="Success"){
-		response.style.backgroundColor="#66FF33";
-	}
-	if(response.innerHTML=="Fail")	{
-		response.style.backgroundColor="#FE3A3C";
-	}
-}
-*/
+var socket = io();
+console.log(socket);
